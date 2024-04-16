0e34ec495a9e50d52035adfb8305ca246ea8df43
digiweb202 <digiwebnjoygroup@gmail.com>
2024-04-16 16:45:36 +0530

    16-04-24(0)

---
+++ app/src/main/java/com/container/imageslider/ImageAdapter.java
@@ -0,0 +1,48 @@
+package com.container.imageslider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private List<String> imageUrls;
+
+    public ImageAdapter(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        String imageUrl = imageUrls.get(position);
+        Picasso.get().load(imageUrl).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewlist);
+        }
+    }
+}
--- app/src/main/java/com/container/imageslider/MainActivity.java
+++ app/src/main/java/com/container/imageslider/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +17,18 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.text.HtmlCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -239,6 +247,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+
+
+        // Your HTML string containing multiple image tags
+        String htmlStringbanner = "<h6>GENERAL INFORMATION</h6>" +
+                "<b>Colour:</b> Blue<br/><br/>" +
+                "<b>Series:</b> XIAOMI A SERIES<br/><br/>" +
+                "<b>Model:</b> A2<br/><br/>" +
+                "<b>Brand:</b> Xiaomi<br/><br/><br/><br/>" +
+
+                "<h6>PHONE HARDWARE & STORAGE</h6>" +
+                "<b>Internal Storage:</b> 64 GB<br/><br/>" +
+                "<b>Processor:</b> MediaTek Helio G36<br/><br/>" +
+                "<b>Storage Type:</b> microSD Card<br/><br/><br/><br/>" +
+
+                "<h6>PHONE OS</h6>" +
+                "<b>Operating System:</b> Andriod 12<br/><br/><br/><br/>" +
+
+                "<h6>NETWORK & INTER-DEVICE CONNECTIVITY</h6>" +
+                "<b>Operating System Type:</b> Android<br/><br/>" +
+                "<b>SIM Type:</b> Dual SIM<br/><br/>" +
+                "<b>4G:</b> Yes - LTE<br/><br/>" +
+                "<b>5G:</b> No<br/><br/><br/><br/>" +
+                "<img src=\"https://img.freepik.com/premium-vector/product-advertising-hero-image-header-layout_1302-21013.jpg\" />" +
+                "<img src=\"https://i.pinimg.com/736x/c7/5a/f7/c75af7e6c5287ccdd17f7c95cd50be91.jpg\" />" +
+                "<img src=\"https://www.bajajmall.in/content/dam/emistoremarketplace/index/10-10-22/geetanjali/mobile-phones-diwali-page/big-banner/desk/MCLP_Row3_1_BigBanner_Desk_OnePlus10Pro_PDP_B2B.jpg\" />"+
+                "<img src=\"https://www.bajajmall.in/content/dam/emistoremarketplace/index/10-10-22/geetanjali/mobile-phones-diwali-page/big-banner/desk/MCLP_Row5_1_BigBanner_Desk_vivoT15G_PDP_B2B.jpg\" />";
+
+        // Extract image URLs using regex
+        List<String> imageUrls = extractImageUrls(htmlStringbanner);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ImageAdapter adapters = new ImageAdapter(imageUrls);
+        recyclerView.setAdapter(adapters);
+
     }
     private void toggleTextView() {
         if (isExpanded) {
@@ -352,5 +398,16 @@ public class MainActivity extends AppCompatActivity {
                 imageView = itemView.findViewById(R.id.imageView);
             }
         }
+    }
+    private List<String> extractImageUrls(String htmlString) {
+        List<String> imageUrls = new ArrayList<>();
+        // Regex pattern to match the src attribute of the img tag
+        Pattern pattern = Pattern.compile("<img\\s+[^>]*?src\\s*=\\s*['\"]([^'\"]*?)['\"][^>]*?>");
+        Matcher matcher = pattern.matcher(htmlString);
+        while (matcher.find()) {
+            String imageUrl = matcher.group(1); // Extract the URL from each match
+            imageUrls.add(imageUrl);
+        }
+        return imageUrls;
     }
 }
